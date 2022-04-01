e6c84d9d0192b10ce81ecbeeb46bdf375d738560
gb-titi <tristanmatteoli@gmail.com>
2022-04-01 16:43:34 +0200

    fix de l'ajout du role dans la creation user

--- API_Auth/app/controllers/auth.controller.js
+++ API_Auth/app/controllers/auth.controller.js
@@ -11,7 +11,8 @@ exports.signup = (req, res) => {
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8)
+    password: bcrypt.hashSync(req.body.password, 8),
+    status: "open"
   });
   user.save((err, user) => {
     if (err) {
@@ -29,6 +30,13 @@ exports.signup = (req, res) => {
             return;
           }
           user.roles = roles.map(role => role._id);
+          user.save(err => {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
+            }
+            res.send({ message: "L'utilisateur a bien été enregistré!" });
+          });
         }
       );
     } else {
@@ -38,31 +46,16 @@ exports.signup = (req, res) => {
           return;
         }
         user.roles = [role._id];
+        user.save(err => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+          res.send({ message: "L'utilisateur a bien été enregistré!" });
+        });
       });
     }
-    let status = req.body.status;
-    if(req.body.status) {
-      switch (status) {
-        case 'open':
-          console.log("Status: open")
-        case 'close':
-          console.log("Status: close")
-          break;
-        default:
-          res.send({ message: "Veuillez rentrer un role valide (open/close), ou bien aucun role. Il sera \"open\" automatiquement" });
-      }
-    } else {
-      status = "open";
-    };
-    user.status = status;
-    user.save(err => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-      res.status(201).send({ message: "L'utilisateur a bien été enregistré!" });
-    });
-  })
+  });
 };
 exports.signin = (req, res) => {
   User.findOne({
@@ -101,8 +94,8 @@ exports.signin = (req, res) => {
         email: user.email,
         roles: authorities,
         accessToken: token,
-        refreshToken: refreshToken,
-        status: user.status
+        refreshToken: refreshToken
+
       });
     });
 };
@@ -120,7 +113,7 @@ exports.refreshToken = async (req, res) => {
     }
     if (RefreshToken.verifyExpiration(refreshToken)) {
       RefreshToken.findByIdAndRemove(refreshToken._id, { useFindAndModify: false }).exec();
-
+      
       res.status(403).json({
         message: "Refresh token was expired. Please make a new signin request",
       });
--- API_Auth/app/routes/auth.routes.js
+++ API_Auth/app/routes/auth.routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     next();
   });
   app.post(
-    "/api/auth/signup",
+    "/api/account",
     [
       verifySignUp.checkDuplicateUsernameOrEmail,
       verifySignUp.checkRolesExisted,
